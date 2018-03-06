56100e20f00b91b44c7d89dacd47467a2af55a87
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2018-03-06 08:33:33 +0100

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -4,14 +4,13 @@ import logging
 import logstash
 import sys
 import requests
+import json
 
 if 'LOG_HOST' not in os.environ:
     raise(Exception("LOG_HOST NOT DEFINED"))
 
 host = os.environ['LOG_HOST']
 API_ENDPOINT = os.environ['LOG_HOST']
-
-
 
 test_logger = logging.getLogger('python-logstash-logger')
 test_logger.setLevel(logging.INFO)
@@ -22,9 +21,9 @@ app = Flask(__name__)
 def log_request(req):
     
     data1 = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-
+    #payload = {'number': 2, 'value': 1}
     datak = {'honeypotsourceip':data1}
-    requests.post(url = API_ENDPOINT, data = datak)
+    requests.post(API_ENDPOINT, json.dumps(datak))
     
     extra = {
         'ip': request.environ.get('HTTP_X_REAL_IP', request.remote_addr),
@@ -39,7 +38,7 @@ def log_request(req):
 
 def honey(path):
     log_request(request)
-    return jsonify({'result': 'ok'})
+    return jsonify({'result': 'okplus'})
     # Test to return the client ip
     # return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
 
